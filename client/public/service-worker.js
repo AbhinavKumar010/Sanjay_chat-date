f3a2d319d535f0fe4d6caf04d1539a06e4ad4c53
AbhinavKumar010 <michaelabhinavthecloud@gmail.com>
2026-05-17 17:30:26 +0530

    djvhjk'

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -4,8 +4,8 @@ const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/icon-192.svg.jpeg',
-  '/icon-512.svg.jpeg',
+  '/icon-192.png',
+  '/icon-512.png',
 ];
 
 self.addEventListener('install', (event) => {
